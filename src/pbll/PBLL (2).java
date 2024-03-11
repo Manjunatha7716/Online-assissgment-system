1614ecabd67173577481299f44333509fca5bd18
Manjunatha m <162434806+Manjunatha7716@users.noreply.github.com>
2024-03-11 18:22:06 +0530

    Add files via upload

---
+++ src/pbll/PBLL (2).java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbll;
+
+import vaccineinfo.uLogin;
+
+/**
+ *
+ * @author DELL
+ */
+public class PBLL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new uLogin().setVisible(true);
+    }
+    
+}
